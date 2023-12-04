73c2ec469d5860ff3e66726269fea270287ed8ba
profplum700 <profplum700@users.noreply.github.com>
2023-12-04 23:44:27 +0000

    unfinished tests

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # appium-codere
+
+Appium tests for the login functionality of the Codere test app.
+
+## Development
+
+- Set up Appium Inspector to discover element locators
+
+## Set Up
+
+- Run the Appium server locally with `appium --base-path /wd/hub`
+- Run the tests with `npm run tests`
+- Requires a _.env_ file to exist at the root with the login credentials:
+  ```
+  USERNAME=username
+  PASSWORD=password
+  ```
---
+++ test/specs/login.e2e.js
@@ -0,0 +1,51 @@
+import { config } from 'dotenv';
+import { remote } from 'webdriverio';
+
+config();
+
+describe('My Tests', function () {
+    this.timeout(60000);
+    let driver;
+
+    before(async function () {
+        driver = await remote({
+            path: '/wd/hub',
+            port: 4723,
+            capabilities: {
+                platformName: 'Android',
+                'appium:deviceName': 'emulator-5554',
+                'appium:appActivity': '.MainActivity',
+                'appium:automationName': 'UiAutomator2',
+                'appium:appPackage': 'spain.codere.apuestas',
+                'appium:appActivity': '.MainActivity'
+            }
+        });
+
+        const titleBar = await driver.$('android=new UiSelector().resourceId("id/nav")');
+        await titleBar.waitForDisplayed(30000);
+
+        let cookiesPopup = await driver.$('android=new UiSelector().text("Configuración de cookies")');
+        if (await cookiesPopup.isDisplayed()) {
+            let acceptButton = await driver.$('android=new UiSelector().text("ACEPTAR")');
+            await acceptButton.click();
+        }
+    });
+
+    after(async function () {
+        await driver.deleteSession();
+    });
+
+    it('should log in with valid credentials', async function () {
+        await driver.$('android=new UiSelector().resourceId("spain.codere.apuestas:id/username")').setValue(process.env.USERNAME);
+        await driver.$('android=new UiSelector().resourceId("spain.codere.apuestas:id/password")').setValue(process.env.PASSWORD);
+        await driver.$('android=new UiSelector().text("Login")').click();
+    
+        let successMessage = await driver.$('android=new UiSelector().text("Success")');
+        await expect(successMessage).toExist();
+    });
+});
+
+
+//   ("should refuse log in with valid username and invalid password")
+//   ("should refuse log in with invalid username and valid password")
+//   ("should refuse log in with invalid username and invalid password")
